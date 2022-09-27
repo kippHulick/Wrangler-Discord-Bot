77977e9558493cf5c000cd32894ad18ab0cd0fb7
kipp <kipphulick@gmail.com>
2022-09-27 14:56:14 -0500

    fixed console logs

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,7 +1,6 @@
 module.exports = {
 	name: 'interactionCreate',
 	async execute(interaction) {
-        console.log({interaction})
 		if (!interaction.isChatInputCommand()) return;
 
         const command = interaction.client.commands.get(interaction.commandName);
