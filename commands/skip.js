fb8969050b0754e36a3abbbd5bd62509f46d82c9
kipp <kipphulick@gmail.com>
2022-10-26 17:57:35 -0500

    fixing skip

--- commands/skip.js
+++ commands/skip.js
@@ -7,7 +7,7 @@ module.exports = {
 
     execute: async (message, args) => {
       const queue = message.client.distube.getQueue(message)
-      if (!queue.autoplay && queue.songs.length <= 1) return
+      if (!message.client.distube.queue.autoplay && queue.songs.length <= 1) return
       if (!queue) return message.channel.send(`There is nothing in the queue buddy!`)
       try {
         const song = await queue.skip()
