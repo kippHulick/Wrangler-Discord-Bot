3b21900915919685e2bec453d93f4298bcf0063e
kipp <kipphulick@gmail.com>
2022-11-09 11:06:32 -0600

    fixed security leak

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('node:path')
 const { DisTube } = require('distube')
 const { DiscordTogether } = require('discord-together')
 require('dotenv').config()
+const secrets = require('./src/utils/secrets')
 
 const { 
 	Client,
@@ -47,7 +48,7 @@ client.distube = new DisTube(client, {
 		new SoundCloudPlugin(),
 		new YtDlpPlugin()
 	],
-	youtubeCookie: 'SID=QAjdj7HovSpi-r5jq0AWB4A1PE82F0p40GoKbfqjUiRqLEaTBNN4R5ySpcE-UI3mo3GcYA.; __Secure-1PSID=QAjdj7HovSpi-r5jq0AWB4A1PE82F0p40GoKbfqjUiRqLEaTEAqJd13rXGm8IOdpxTXkbA.; __Secure-3PSID=QAjdj7HovSpi-r5jq0AWB4A1PE82F0p40GoKbfqjUiRqLEaTAmo4M_qxBR0GUuoghqLZig.; HSID=A5JkgcK4xRHzwbiNw; SSID=Akl_fV1Y07ymCpp-5; APISID=dHZtVqIMbzvVwcg-/Aq0sLYl7tMe_zEApc; SAPISID=qZGO2w89gCT9-bAb/AIwCZsuKI5R4cb3W-; __Secure-1PAPISID=qZGO2w89gCT9-bAb/AIwCZsuKI5R4cb3W-; __Secure-3PAPISID=qZGO2w89gCT9-bAb/AIwCZsuKI5R4cb3W-; LOGIN_INFO=AFmmF2swRQIhAIEvYbXkPJuC_avTkLheRpcdAG8GyYP_I_yQ6SHWvc8xAiBjNlpG_HxeEkXgoVKLvkisrggv8MINBTg9VFo2oSVGxQ:QUQ3MjNmeVFzT3FZc3FmWUZjSmVRSUdhTnFXbDlpaGExTV83dnJGbEpfNEQxMVpZLWktWVF0anBGR2xXdXdUd3FxcUhYZUw2ZmJjUERoWmFkQ21RakUwVGtBQ0JIczM0WUxrM1l0NGtreEd4Zl93aTNNQU5PenlhcU9EcS1WVWhvQ2lwUFF2N0ZYbW5aUk5EMGV2dE80TzVuR0dnUG9tZUpB; VISITOR_INFO1_LIVE=0tIQnglQP4E; SIDCC=AIKkIs0lCTkFByA4URYNQOXnLd3jxqv4sx66fRJVi41czFoLnSyHdEPZ8gVAh3zne1C78syzKhM; __Secure-1PSIDCC=AIKkIs09xSG1v6ruX3pmIcit3aHuBFvOiZzSWrI6f2IqBKSlCXjbcRIPpbOaS6VMBIc9mMGlPuc; __Secure-3PSIDCC=AIKkIs2uRi0ieiVgljla83s_JTTSP_kGGfG-p7-KZgSkze70vThSBHgTr3kL1HQlLEeneKC92nI; PREF=tz=America.Chicago&f6=40000000&f4=4000000&f5=20000&f7=100; YT_CL={"loctok":"APLXd66ADn6MXKS3sElLSauoWc1wQD99jXDNwjbnfaCmceYlPoFxKblpsbgHR8HjLUkXTESUqsTrsvEXX0fVQOTeyRLFmg"}; YSC=rGqUdAvDZP0; wide=1'
+	youtubeCookie: secrets.youTubeCookie
 })
 
 client.discordTogether = new DiscordTogether(client);
