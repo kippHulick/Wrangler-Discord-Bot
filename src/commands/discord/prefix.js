779dabf3a1d16a19979df3afd782bac6864e444c
Battlepigg <kipphulick@gmail.com>
2023-08-04 01:42:52 -0500

    fixed prefix bug

--- src/commands/discord/prefix.js
+++ src/commands/discord/prefix.js
@@ -7,7 +7,7 @@ module.exports = {
     },
 
   execute: async (message, args) => {
-    if (!args) return message.channel.send('You need to choose a prefix big guy!')
+    if (args.length <= 0) return message.channel.send('You need to choose a prefix big guy!')
     state.setPrefix(message.guildId, args[0])
     message.channel.send(`Prefix changed to ${args[0]}`)
 
